4d47f32454c52d7464af4d155d748bb400f34af2
Shivansh Singh Bhadouria <shivanshbhadouria7@gmail.com>
2026-02-16 15:25:08 +0530

    Rename LogParserController to MessageParserController

--- src/main/java/com/l3/logparser/controller/LogParserController.java
+++ src/main/java/com/l3/logparser/controller/LogParserController.java
@@ -44,7 +44,7 @@ import java.util.ResourceBundle;
  * Controller for the Message Extractor UI
  * Handles both API and PNR data extraction
  */
-public class LogParserController implements Initializable {
+public class MessageParserController implements Initializable {
 
     @FXML private TextField logDirectoryField;
     @FXML private Button browseButton;
