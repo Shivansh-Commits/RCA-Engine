e34fd06bf80e1740cbfaa395663e50f2b5753a49
Shivansh Singh Bhadouria <shivanshbhadouria7@gmail.com>
2026-02-16 15:23:14 +0530

    Rename LogParserApplication to MessageParserController

--- src/main/java/com/l3/logparser/LogParserApplication.java
+++ src/main/java/com/l3/logparser/LogParserApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
  * Main application class for the Message Extractor module
  * This module extracts API/PNR data from raw log files
  */
-public class LogParserApplication extends Application {
+public class MessageParserController extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
